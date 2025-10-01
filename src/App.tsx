c67f48bdb76816502994bb239de26c28c44099c9
serjo <serjo-study@ya.ru>
2025-10-01 18:27:44 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 // import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import MainPage from './pages/MainPage';
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import OfferPage from "./pages/OfferPage";
 
 function App() {
 
   return (
-    <MainPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/favorites' element={<FavoritesPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='/offer/:id' element={<OfferPage />} />
+
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
