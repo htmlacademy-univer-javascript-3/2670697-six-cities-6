0e67c20caa1220098c3703f25ea08583c1f98370
serjo <serjo-study@ya.ru>
2025-10-02 14:05:37 +0300

    feat: add private route

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,17 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import FavoritesPage from './pages/FavoritesPage/FavoritesPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import OfferPage from './pages/OfferPage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/favorites' element={<FavoritesPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/favorites' element={<FavoritesPage />} />
+        </Route>
+
         <Route path='/login' element={<LoginPage />} />
         <Route path='/' element={<MainPage />} />
         <Route path='/offer/:id' element={<OfferPage />} />
