b3459f2f131964239835ea998e29ad898564e8d4
serjo <serjo-study@ya.ru>
2025-10-09 16:28:53 +0300

    feat: add state isHover for offers card

--- src/components/CityPlaceCard/CityPlaceCard.tsx
+++ src/components/CityPlaceCard/CityPlaceCard.tsx
@@ -1,50 +1,70 @@
 // import { type FC } from 'react';
+import { useState } from 'react';
 import { IBaseOffer } from '../../mocks/offers';
 
 interface CityPlaceCardProps {
   offer: IBaseOffer;
 }
 
-const CityPlaceCard = ({ offer }: CityPlaceCardProps) => (
-  <article className='cities__card place-card'>
-    {
-      offer.isPremium && (
-        <div className='place-card__mark'>
-          <span>Premium</span>
-        </div>
-      )
-    }
+const CityPlaceCard = ({ offer }: CityPlaceCardProps) => {
 
-    <div className='cities__image-wrapper place-card__image-wrapper'>
-      <a href='#'>
-        <img className='place-card__image' src={offer.previewImage} width='260' height='200' alt='Place image' />
-      </a>
-    </div>
-    <div className='place-card__info'>
-      <div className='place-card__price-wrapper'>
-        <div className='place-card__price'>
-          <b className='place-card__price-value'>&euro;{offer.price}</b>
-          <span className='place-card__price-text'>&#47;&nbsp;night</span>
-        </div>
-        <button className='place-card__bookmark-button button' type='button'>
-          <svg className='place-card__bookmark-icon' width='18' height='19'>
-            <use href='#icon-bookmark'></use>
-          </svg>
-          <span className='visually-hidden'>To bookmarks</span>
-        </button>
+  const [isHover, setHover] = useState<boolean>(false);
+
+  const handleMouseEnter = () => {
+    console.log(`Навел на ${offer.id}`);
+    setHover(true);
+  }
+
+  const handleMouseLeave = () => {
+    console.log(`Покинул ${offer.id}`);
+    setHover(false);
+  }
+
+  return (
+    <article 
+      className='cities__card place-card'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {
+        offer.isPremium && (
+          <div className='place-card__mark'>
+            <span>Premium</span>
+          </div>
+        )
+      }
+
+      <div className='cities__image-wrapper place-card__image-wrapper'>
+        <a href='#'>
+          <img className='place-card__image' src={offer.previewImage} width='260' height='200' alt='Place image' />
+        </a>
       </div>
-      <div className='place-card__rating rating'>
-        <div className='place-card__stars rating__stars'>
-          <span style={{ width: `${offer.rating * 100 / 5}%` }}></span>
-          <span className='visually-hidden'>{offer.rating}</span>
+      <div className='place-card__info'>
+        <div className='place-card__price-wrapper'>
+          <div className='place-card__price'>
+            <b className='place-card__price-value'>&euro;{offer.price}</b>
+            <span className='place-card__price-text'>&#47;&nbsp;night</span>
+          </div>
+          <button className='place-card__bookmark-button button' type='button'>
+            <svg className='place-card__bookmark-icon' width='18' height='19'>
+              <use href='#icon-bookmark'></use>
+            </svg>
+            <span className='visually-hidden'>To bookmarks</span>
+          </button>
         </div>
+        <div className='place-card__rating rating'>
+          <div className='place-card__stars rating__stars'>
+            <span style={{ width: `${offer.rating * 100 / 5}%` }}></span>
+            <span className='visually-hidden'>{offer.rating}</span>
+          </div>
+        </div>
+        <h2 className='place-card__name'>
+          <a href='#'>{offer.title}</a>
+        </h2>
+        <p className='place-card__type'>{offer.type}</p>
       </div>
-      <h2 className='place-card__name'>
-        <a href='#'>{offer.title}</a>
-      </h2>
-      <p className='place-card__type'>{offer.type}</p>
-    </div>
-  </article>
-);
+    </article>
+  )
+};
 
 export default CityPlaceCard;
